da5fdd484fc24b97d9d87e00be53e8ea7412f8dd
agamestra <shyshygon@ya.ru>
2015-10-06 11:08:25 +0300

    Work "Tests"

--- 1-sin.py
+++ 1-sin.py
@@ -5,15 +5,15 @@ import lib
 class LibTest(unittest.TestCase):
 
     def test_sin_0(self):
-        self.assertEqual(lib.sin(0), 0)
+        self.assertEqual((lib.sin(0))/1, 0)
 
     def test_sin_plus(self):
-        self.assertEqual(lib.sin(math.pi), 0)
-        self.assertEqual(lib.sin(math.pi/2), 1)
+        self.assertEqual((lib.sin(math.pi))/1, 0)
+        self.assertEqual((lib.sin(math.pi/2))/1, 1)
 
     def test_sin_minus(self):
-        self.assertEqual(lib.sin(-math.pi), 0)
-        self.assertEqual(lib.sin(-math.pi/2), -1)
+        self.assertEqual((lib.sin(-math.pi))/1, 0)
+        self.assertEqual((lib.sin(-math.pi/2))/1, -1)
 
 unittest.main(verbosity=2)
 
